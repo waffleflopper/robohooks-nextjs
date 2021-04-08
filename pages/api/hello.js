1daaa572b1b2905a2ceecabf84322f751aa9a046
Robert <robertbaddeley@gmail.com>
2021-04-09 00:36:43 +0900

    reverted hello api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,15 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-
 export default (req, res) => {
-  res.status(200).json(getUsers())
-}
-
-const getUsers = () => {
-  let userList = [];
-  fetch('http://jsonplaceholder.typicode.com/users')
-  .then(response=> response.json())
-  .then(users => console.log(users))
-  .catch(e => console.log(e));
-  return userList;
+  res.status(200).json({ name: 'John Doe' })
 }
